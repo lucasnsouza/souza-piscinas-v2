1bab959ce0697e40e864dc2d96dd125892eab09d
lucasnsouza <lucasnstj@gmail.com>
2022-08-16 15:53:47 -0300

    feat: slideshow

--- src/pages/Home/Conteudo/index.jsx
+++ src/pages/Home/Conteudo/index.jsx
@@ -1,21 +1,46 @@
-import styles from './Conteudo.module.css';
+import { useEffect, useState } from "react";
+import fotos from "../../../data/fotos.json";
+import styles from "./Conteudo.module.css";
 
 export default function Conteudo() {
-    return (
-        <div className={styles.conteudo}>
-            <div className={styles.apresentacao}>
-                <p>
-                Preparamos um guia rápido e básico para você ficar por dentro do que é mais importante para manter a sua piscina em ordem. Esse guia também pode ajudar você a entender um pouco mais do trabalho do seu piscineiro ou até economizar com a manutenção da piscina.
-                </p>
-            </div>
-            <div className={styles.guia}>
-                <p>O que você procura?</p>
-                <ul>
-                    <li>Manutenção regular</li>
-                    <li>Manutenção peródica</li>
-                    <li>Serviços extras</li>
-                </ul>
-            </div>
+  let [slide, setSlide] = useState(fotos[0]);
+  let [contador, setContador] = useState(0);
+  
+  if (contador === fotos.length) {
+    setContador(0);
+  }
+
+  useEffect(() => {
+    setSlide(fotos[contador]);
+  }, [contador]);
+
+
+  return (
+    <div className={styles.conteudo}>
+      <div className={styles.texto}>
+        <div className={styles.apresentacao}>
+          <h2 className={styles.titulo}><strong>Guia</strong></h2>
+          <hr className={styles.hr}/>
+          <p>
+            Preparamos um guia rápido e básico para você ficar por dentro do que
+            é mais importante para manter a sua piscina em ordem.
+          </p>
         </div>
-    )
+        <div className={styles.guia}>
+          <p>O que você procura?</p>
+          <ul>
+            <li>Manutenção regular</li>
+            <li>Manutenção peródica</li>
+            <li>Serviços extras</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.slides}>
+        <div className={styles.fotos}>
+            <img src={slide.path} alt={slide.description} />
+            <button onClick={() => setContador(contador + 1)}>muda</button>
+        </div>
+      </div>
+    </div>
+  );
 }
