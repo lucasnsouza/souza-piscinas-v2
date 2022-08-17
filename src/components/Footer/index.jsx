3a24f735d66d88f8a048b0dc3663cff66b4c9b34
lucasnsouza <lucasnstj@gmail.com>
2022-08-17 16:33:26 -0300

    feat: working with svg

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,8 @@
 import Brand from "../Brand";
 import styles from "./Footer.module.css";
+import { ReactComponent as Phone } from '../../assets/phone.svg'; 
+import { ReactComponent as Mail } from '../../assets/mail.svg'; 
+import { ReactComponent as Clock } from '../../assets/clock.svg'; 
 
 export default function Footer() {
     const anoAtual = new Date().getFullYear();
@@ -8,15 +11,24 @@ export default function Footer() {
       <div className={styles.cartao}>
           <Brand />
           <ul className={styles.lista}>
-            <li><strong>Contato</strong></li>
-            <li>email@email.com</li>
-            <li>(14) 91122-3344</li>
-            <li>Atendemos de qua a sex das 08:00 às 17:00</li>
+            <li><strong>contato</strong></li>
+            <li className={styles.item}>
+                <div className={styles.svg}><Mail/></div>
+                <div>email@email.com</div>
+            </li>
+            <li className={styles.item}>
+                <div className={styles.svg}><Phone/></div>
+                <div>(14) 91122-3344</div>
+            </li>
+            <li className={styles.item}>
+                <div className={styles.svg}><Clock/></div>
+                <div>atendemos de qua a sex das 08:00 às 17:00</div>
+            </li>
           </ul>
       </div>
       <hr />
       <div className={styles.developer}>
-        <span>Desenvolvido por Lucas Souza | 2021-{anoAtual}</span>
+        <span>criado por lucas souza | 2021-{anoAtual}</span>
       </div>
     </div>
   );
