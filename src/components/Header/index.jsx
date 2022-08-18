2d89c48366d1b308e21d715f4ebb16de8f998fbf
lucasnsouza <lucasnstj@gmail.com>
2022-08-18 15:11:20 -0300

    feat: ajustando header mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,36 +1,68 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 import Brand from "../Brand";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const rotas = [
     {
-      label: 'início',
-      to: '/'
+      label: "início",
+      to: "/",
     },
     {
-      label: 'serviços',
-      to: '/serviços'
+      label: "serviços",
+      to: "/serviços",
     },
     {
-      label: 'sobre nós',
-      to: '/sobre'
-    }
+      label: "sobre nós",
+      to: "/sobre",
+    },
   ];
+
+  //começa como falso já que o botão só deve aparecer em telas mobile
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [tamanhoDaTela, setTamanhoDaTela] = useState(window.innerWidth);
+
+  const btnNavToggle = () => {
+    //a função dá um valor booleano contrario
+    //if is false its set for true and vice versa
+    setToggleMenu(!toggleMenu);
+  };
+
+  //monitora o tamanho da tela e garante que tamanhoDaTela
+  //seja atualizado sempre que houver um resize
+  useEffect(() => {
+    const changeWidth = () => {
+      setTamanhoDaTela(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
 
   return (
     <nav className={styles.navBar}>
       <div className={styles.menu}>
-        <Link to="/" className={styles.brand}>
-        <Brand />
+        <Link to='/' className={styles.brand}>
+          <Brand />
         </Link>
         <ul className={styles.lista}>
-          {rotas.map((rota, index) => (
-            <li key={index} className={styles.item}>
-              <Link to={rota.to} className={styles.link}>{rota.label}</Link>
-            </li>
-          ))}
+          {/* exibe a ul do menu se o toggle for true ou a tela for larga o suficiente */}
+          {(toggleMenu || tamanhoDaTela > 500) &&
+            rotas.map((rota, index) => (
+              <li key={index} className={styles.item}>
+                <Link to={rota.to} className={styles.link}>
+                  {rota.label}
+                </Link>
+              </li>
+            ))}
         </ul>
+        <button onClick={btnNavToggle} className={styles.btnNav}>
+          <i className='fa-solid fa-bars fa-xl'></i>
+        </button>
       </div>
     </nav>
   );
