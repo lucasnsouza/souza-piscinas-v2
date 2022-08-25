9201b736b472b64c8491e330cae214e5fbc5ce23
lucasnsouza <lucasnstj@gmail.com>
2022-08-25 12:31:03 -0300

    hash link servicos

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -1,25 +1,43 @@
-import ServicesDescription from '../../components/ServicesDescription';
-import styles from './Services.module.css';
-import listaServicos from '../../data/description.json';
+import ServicesDescription from "../../components/ServicesDescription";
+import styles from "./Services.module.css";
+import listaServicos from "../../data/description.json";
+import { HashLink } from "react-router-hash-link";
 
 export default function Services() {
-    return (
-        <main>
-            <div className={styles.capa}>
-                <div className={styles.conteudo}>
-                    <h1 className={styles.titulo}>O que está envolvido na manutenção da sua piscina?</h1>
-                    <h3>Esse guia vai abranger alguns dos principais aspectos necessários para manter uma piscina em ordem</h3>
-                    <ul className={styles.lista}>
-                        <li className={styles.itemGuia}>Veja como funciona ...</li>
-                        <li className={styles.itemGuia}>limpeza semanal</li>
-                        <li className={styles.itemGuia}>manutenção periódica</li>
-                        <li className={styles.itemGuia}>produtos utilizados</li>
-                    </ul>
-                </div>
-            </div>
-            {listaServicos.map((servico) => (
-                <ServicesDescription lista={servico} key={servico.id}/>
-            ))}
-        </main>
-    );
+  return (
+    <main>
+      <div className={styles.capa}>
+        <div className={styles.conteudo}>
+          <h1 className={styles.titulo}>
+            O que está envolvido na manutenção da sua piscina?
+          </h1>
+          <h3>
+            Esse guia vai abranger alguns dos principais aspectos necessários
+            para manter uma piscina em ordem
+          </h3>
+          <ul className={styles.lista}>
+            <li className={styles.itemGuia}>Veja como funciona ...</li>
+            <li className={styles.itemGuia}>
+              <HashLink smooth to='/souza-piscinas-v2/servicos/#regular'>
+                limpeza semanal
+              </HashLink>
+            </li>
+            <li className={styles.itemGuia}>
+              <HashLink smooth to='/souza-piscinas-v2/servicos/#extra'>
+                manutenção periódica
+              </HashLink>
+            </li>
+            <li className={styles.itemGuia}>
+              <HashLink smooth to='/souza-piscinas-v2/servicos/#produtos'>
+                produtos utilizados
+              </HashLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {listaServicos.map((servico) => (
+        <ServicesDescription lista={servico} key={servico.id} />
+      ))}
+    </main>
+  );
 }
